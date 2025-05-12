09513b2845e372cc9361bc2e648d9c5011cfe569
GEORGE SENIOR MENSAH <georgeseniormensah@GEORGEs-MacBook-Pro-2.local>
2025-05-12 12:29:12 +0000

    changes made

--- server/index.ts
+++ server/index.ts
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 9098;
+  const port = 9097;
   server.listen({
     port,
     // host: "0.0.0.0",
