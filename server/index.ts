d95443b2d9c44e6025d9c5d1319b506f74ad118c
GEORGE SENIOR MENSAH <georgeseniormensah@GEORGEs-MacBook-Pro-2.local>
2025-05-12 17:15:47 +0000

    changes made

--- server/index.ts
+++ server/index.ts
@@ -62,8 +62,8 @@ app.use((req, res, next) => {
   const port = 9097;
   server.listen({
     port,
-    // host: "0.0.0.0",
-    host: "localhost",
+    host: "0.0.0.0",
+    // host: "localhost",
     // reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
